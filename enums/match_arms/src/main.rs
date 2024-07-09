87fa893c6f9e3c160435415bbae6a0f43a3ec0fa
Kartikhub <sharmakartik66@gmail.com>
2024-07-10 00:01:48 +0530

    added files related to matches, arms and if let

---
+++ enums/match_arms/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    let new_coin = Coin::Athanni(IndState::Assam);
+    let value: u8 = value_in_coins(new_coin);
+    println!("value of the coin is {value}");
+
+    
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {max}");
+    }
+}
+// It will panic as match is exahustive i.e., all the variants of enums
+// must be cover while matching. In this case None is not present.
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(i) => Some(i + 1),
+    }
+}
+
+
+
+fn value_in_coins(coin: Coin) -> u8 {
+    match coin {
+        Coin::Ekanni => 1,
+        Coin::Duanni => 2,
+        Coin::Chawanni => 4,
+        Coin::Athanni(state) => {
+            println!("State quarter from {state:?}");
+            8
+        }
+    }
+}
+
+#[derive(Debug)]
+enum IndState {
+    Andhra_Pradesh,
+    Arunachal_Pradesh,
+    Assam,
+    Bihar,
+}
+
+enum Coin {
+    Ekanni,
+    Duanni,
+    Chawanni,
+    Athanni(IndState),
+}
--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
     let m = Message::Write(String::from("MJ"));
     m.call();
+
+    // Explictly provide type otherwise -> panic
+    let absent_num: Option<i32> = None;
+    
+    let i: Option<i32> =None;
+    println!("value of i is {i:?}");
+
 }
 
+// enums and variants
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
