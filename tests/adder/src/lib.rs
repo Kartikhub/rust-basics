68c35fb005d3cdee54be760f5c7272a855508aaa
Kartikhub <sharmakartik66@gmail.com>
2024-08-01 23:26:33 +0530

    added tests

---
+++ tests/adder/src/lib.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: Rectangle) -> bool {
+        self.width < other.width && self.height > other.height
+    }
+}
+
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test] // add test attribute
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 10,
+            height: 20
+        };
+
+        let smaller = Rectangle {
+            width: 3,
+            height: 5
+        };
+        assert!(larger.can_hold(smaller));
+    }
+
+    // #[test]
+    // fn another() {
+    //     panic!("Fail the test");
+    // }
+}
